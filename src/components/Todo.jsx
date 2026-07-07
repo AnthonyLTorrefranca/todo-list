9d0977f0eb5c31356c75511102de00f71e748e3f
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-07 18:21:48 +0800

    feat: added a move down and up

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,55 +6,64 @@ export default function Todo() {
   const [task, setTask] = useState("")
   const [taskList, setTaskList] = useState([])
 
+  function handleChange(e){
+    setTask(e.target.value)
+  }
   function handleSubmit(e){
     e.preventDefault()
     if (task.trim() === ""){
       setBlank(prev=>({...prev, idle: false, input: true}))
       setTimeout(() => {
         setBlank(prev=>({...prev, idle: true, input: false}))
-      }, 1500);
+      }, 1000);
       return
     }
-    setTaskList(prev=> [...prev, task])
+    setTaskList(prev=>[...prev, task])
     setTask("")
   }
-  function handleChange(e){
-    setTask(e.target.value)
-  }
   function handleDelete(index){
-    const updatedTask = 
-    taskList.filter((_, i) => 
-      i !== index
-    )
-    setTaskList(updatedTask)
+    const updatedTasks = taskList.filter((_,i)=> i !== index);
+    setTaskList(updatedTasks)
   }
-  useEffect(()=>{
-    console.log(taskList)
-  }, [taskList])
-return (
-  <>
-    <section className='todo_container'>
-      <form onSubmit={handleSubmit}>
-        {blank.idle && <h1>Add your bucketlist:</h1>}
-        {blank.input && <span className='alertUser'>You need to enter valid bucketlist</span>}
-        <section className='taskGroup'>
-          <input type="text" placeholder="Enter here"
+  function moveUp(index){
+    const updatedTasks = [...taskList]
+    if (index > 0){
+      [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]]
+    }
+    setTaskList(updatedTasks)
+  }
+  function moveDown(index){
+    const updatedTasks = [...taskList]
+    if (index < taskList.length - 1){
+      [updatedTasks[index+1], updatedTasks[index]] = [updatedTasks[index],updatedTasks[index+1]]
+    }
+    setTaskList(updatedTasks)
+  }
+  useEffect(()=>{console.log(taskList)},[taskList])
+  return (
+    <section>
+      <form className="todo_container" onSubmit={handleSubmit}>
+        {blank.idle && <h1>TO-DO LIST</h1>}
+        {blank.input && <span className='alertUser'>Please enter valid task!</span>}
+        <section className="taskGroup">
+          <input type="text" placeholder="Add your task here:"
             value={task} onChange={handleChange} />
           <button type="submit">ADD</button>
         </section>
+        <section className='tasks'>
+          <ul>
+            {taskList.map((task, index)=>{
+              return <li key={index}>{task}
+              <section className="listBtn">
+                <button type="button" onClick={()=> moveUp(index)}>☝️</button>
+                <button type="button" onClick={()=> moveDown(index)}>👇</button>
+                <button type="button" onClick={()=> handleDelete(index)}>❌</button>
+              </section>
+              </li>
+            })}
+          </ul>
+        </section>
       </form>
-      <section className="tasks">
-        {taskList.map((task, index)=>[
-          <li key={index}>{task}
-            <section className='listBtn'>
-              <button>☝️</button>
-              <button>👇</button>
-              <button onClick={()=> handleDelete(index)}>❌</button>
-            </section>
-          </li>
-        ])}
-      </section>
     </section>
-  </>
   )
 }
