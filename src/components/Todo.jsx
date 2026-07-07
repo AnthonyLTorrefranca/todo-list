b363e527838b7559616c7e39e04a3387bb83be70
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-07 13:06:10 +0800

    memory test

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,53 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Todo.css'
 
 export default function Todo() {
-  const [blank, setBlank] = useState(false);
-  const [task,setTask] = useState("")
-  const [taskList,setTaskList] = useState([])
-  function handleChange(e){
-    setTask(e.target.value)
-  }
+  const [blank, setBlank] = useState({idle: true, input: false})
+  const [task, setTask] = useState("")
+  const [taskList, setTaskList] = useState([])
+
   function handleSubmit(e){
     e.preventDefault()
     if (task.trim() === ""){
-      setBlank(true)
+      setBlank(prev=>({...prev, idle: false, input: true}))
       setTimeout(() => {
-        setBlank(false)
-      }, 2500);
+        setBlank(prev=>({...prev, idle: true, input: false}))
+      }, 1500);
       return
     }
-    setTaskList(prev=>[...prev, task])
+    setTaskList(prev=> [...prev, task])
     setTask("")
   }
+  function handleChange(e){
+    setTask(e.target.value)
+  }
   function handleDelete(index){
-    const updatedTask = taskList.filter((_,i)=> i !== index);
+    const updatedTask = 
+    taskList.filter((_, i) => 
+      i !== index
+    )
     setTaskList(updatedTask)
   }
+  useEffect(()=>{
+    console.log(taskList)
+  }, [taskList])
 return (
   <>
     <section className='todo_container'>
       <form onSubmit={handleSubmit}>
-        <h1>Add your bucketlist:</h1>
-        {blank? <span className='alertUser'>You need to enter valid bucketlist</span>: null }
+        {blank.idle && <h1>Add your bucketlist:</h1>}
+        {blank.input && <span className='alertUser'>You need to enter valid bucketlist</span>}
         <section className='taskGroup'>
           <input type="text" placeholder="Enter here"
-           value={task} onChange={handleChange} />
+            value={task} onChange={handleChange} />
           <button type="submit">ADD</button>
         </section>
       </form>
       <section className="tasks">
-        <ul>
-          {taskList.map((task, index)=>(
-            <li key={index}>{task}
-            <section className="listBtn">
-              <button>👆</button>
+        {taskList.map((task, index)=>[
+          <li key={index}>{task}
+            <section className='listBtn'>
+              <button>☝️</button>
               <button>👇</button>
-              <button onClick={()=>handleDelete(index)}>❌</button>
+              <button onClick={()=> handleDelete(index)}>❌</button>
             </section>
-            </li>
-          ))}
-        </ul>
+          </li>
+        ])}
       </section>
     </section>
   </>
