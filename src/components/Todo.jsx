7344aa3cb6b8d98e7e47079b9820badb0ee5c57b
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-08 15:30:45 +0800

    refactor: added slight feature and written this from memory.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react'
 import './Todo.css'
 
 export default function Todo() {
-  const [blank, setBlank] = useState({idle: true, input: false})
-  const [task, setTask] = useState("")
-  const [taskList, setTaskList] = useState([])
+  const [blank, setBlank] = useState({idle: true, input: false});
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
 
   function handleChange(e){
     setTask(e.target.value)
@@ -12,32 +12,35 @@ export default function Todo() {
   function handleSubmit(e){
     e.preventDefault()
     if (task.trim() === ""){
-      setBlank(prev=>({...prev, idle: false, input: true}))
+      setBlank(prev => ({...prev, idle:false, input:true}))
       setTimeout(() => {
-        setBlank(prev=>({...prev, idle: true, input: false}))
-      }, 1000);
+        setBlank(prev => ({...prev, idle:true, input:false}))
+      }, 1400);
       return
     }
     setTaskList(prev=>[...prev, task])
     setTask("")
   }
   function handleDelete(index){
-    const updatedTasks = taskList.filter((_,i)=> i !== index);
-    setTaskList(updatedTasks)
+    const updatedTask = taskList.filter((_,i)=> i !== index);
+    setTaskList(updatedTask)
   }
   function moveUp(index){
-    const updatedTasks = [...taskList]
+    const updatedTask = [...taskList]
     if (index > 0){
-      [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]]
+      [updatedTask[index-1], updatedTask[index]] = [updatedTask[index], updatedTask[index - 1]] 
+      setTaskList(updatedTask)
+      return
     }
-    setTaskList(updatedTasks)
   }
   function moveDown(index){
-    const updatedTasks = [...taskList]
-    if (index < taskList.length - 1){
-      [updatedTasks[index+1], updatedTasks[index]] = [updatedTasks[index],updatedTasks[index+1]]
+    const updatedTask = [...taskList];
+    if (index < updatedTask.length - 1){
+      [updatedTask[index + 1], updatedTask[index]] = [updatedTask[index], updatedTask[index + 1]]
+      setTaskList(updatedTask)
+      return
     }
-    setTaskList(updatedTasks)
+    console.log("nah")
   }
   useEffect(()=>{console.log(taskList)},[taskList])
   return (
@@ -46,21 +49,19 @@ export default function Todo() {
         {blank.idle && <h1>TO-DO LIST</h1>}
         {blank.input && <span className='alertUser'>Please enter valid task!</span>}
         <section className="taskGroup">
-          <input type="text" placeholder="Add your task here:"
+          <input type="text" placeholder="Add your task here:" 
             value={task} onChange={handleChange} />
-          <button type="submit">ADD</button>
+          <button type="button" type="submit">ADD</button>
         </section>
         <section className='tasks'>
           <ul>
-            {taskList.map((task, index)=>{
-              return <li key={index}>{task}
+            {taskList.map((task, index)=>[ <li key={index}> {task}
               <section className="listBtn">
-                <button type="button" onClick={()=> moveUp(index)}>☝️</button>
-                <button type="button" onClick={()=> moveDown(index)}>👇</button>
-                <button type="button" onClick={()=> handleDelete(index)}>❌</button>
+                <button type="button" onClick={()=>moveUp(index)}>☝️</button>
+                <button type="button" onClick={()=>moveDown(index)}>👇</button>
+                <button type="button" onClick={()=>handleDelete(index)}>❌</button>
               </section>
-              </li>
-            })}
+            </li> ])}
           </ul>
         </section>
       </form>
