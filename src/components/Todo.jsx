f2566dd7b57fa36d7c8731d5b26e08f0c328088d
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-09 16:22:47 +0800

    refactor: alert message for the user

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,42 +2,54 @@ import React, {useState} from 'react'
 import './Todo.css'
 
 export default function Todo(){
-    const [blank,setBlank] = useState("idle")
+    const [alert,setAlert] = useState("idle")
     const [task,setTask] = useState("")
     const [taskList, setTaskList] = useState([])
 
     function handleSubmit(e){
         e.preventDefault()
-        if (task.trim() === ""){
-            setBlank("")
+            // blank
+        if (task.trim()===""){
+            setAlert("blank")
+            return
+            // duplicate
+        } else if (taskList){
             return
         }
-        console.log(task)
-        setTaskList(prev =>[...prev, task])
+            // unique key ngani
+        const newTask = {id: crypto.randomUUID, text: task}
+        setTaskList(prev=>[...prev, newTask])
     }
     function handleChange(e){
-        e.preventDefault()
         setTask(e.target.value)
-        setBlank("idle")
+        setAlert("idle")
+    }
+    function handleDelete(index){
+    }
+    function moveUp(index){
+        const updatedTask = [...taskList]
+        console.log("test up ngani")
+    }
+    function moveDown(index){
+        const updatedTask = [...taskList]
     }
 return (<section className='todo_container'>
     <form onSubmit={handleSubmit}>
-        {blank && <h1>Welcome back!</h1>}
-        {!blank && <span className='alertUser'>Enter valid task!</span>}
+        {alert === "idle" && <h1>Welcome back!</h1>}
+        {alert === "blank" && <span className='alertUser'>Enter valid task!</span>}
+        {alert === "duplicate" && <span className='alertUser'>This task already exist!</span>}
         <section className="taskGroup">
             <input type="text" placeholder="Add task..."
-                name="task" onChange={handleChange} />
+                name="task" value={task} onChange={handleChange} />
             <button>ADD</button>
         </section>
         <section className="tasks">
             <ul>
-                {taskList.map((task, index)=> <li key={index}> {task}
-                    <section className="listBtn">
-                        <button type="button">☝️</button>
-                        <button type="button">👇</button>
-                        <button type="button">❌</button>
-                    </section>
-                </li>)}
+                <section className="listBtn">
+                    <button type="button" onClick={()=> moveUp(index)}>☝️</button>
+                    <button type="button" onClick={()=> moveDown(index)}>👇</button>
+                    <button type="button" onClick={()=>handleDelete(index)}>❌</button>
+                </section>
             </ul>
         </section>
     </form>
