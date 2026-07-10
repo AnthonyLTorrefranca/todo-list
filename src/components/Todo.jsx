1b3e3aff41bb6baf34294f4fbd9546d077b2e0ea
Anthony L. Torrefranca <anthony.torrefranca@gmail.com>
2026-07-10 20:14:03 +0800

    refactor: cleaned the function handleSubmit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,69 +1,74 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Todo.css'
 
 export default function Todo(){
-    const [alert, setAlert] = useState("idle")
+    const [alert, setAlert] = useState("idle");
     const [task, setTask] = useState("")
     const [taskList, setTaskList] = useState([])
     function handleSubmit(e){
         e.preventDefault()
-        const taskContent = task.trim()
-        if (taskContent === ""){
+        const newTask = {id: crypto.randomUUID(), text: task.trim()};
+        if (newTask.text === ""){
             setAlert("blank")
             return
-        } else if (taskList.some(item=> item.text.toLowerCase() === taskContent.toLowerCase())){
+        } else if (taskList.some(item=> item.text.toLowerCase() === task.trim().toLowerCase())){
             setAlert("duplicate")
             return
         }
-        const newTask = { id: crypto.randomUUID(), text: task}
-        setTaskList(prev=> [...prev, newTask])
+        setTaskList(prev=>[...prev, newTask])
         setTask("")
     }
     function handleChange(e){
         setTask(e.target.value)
         setAlert("idle")
     }
-    function handleDelete(index){
-        const updatedTask= taskList.filter((_,i)=> i !==index)
+    function handleDelete(id){
+        const updatedTask = taskList.filter(item=> item.id !== id)
         setTaskList(updatedTask)
     }
-    function moveUp(index){
-        const updatedTask = [...taskList]
+    function moveUp(id){
+        const updatedTask = [...taskList];
+        const index = updatedTask.findIndex(item=> item.id === id);
         if (index > 0){
-            [updatedTask[index], updatedTask[index-1]] = [updatedTask[index-1], updatedTask[index]]
+            [updatedTask[index],updatedTask[index-1]] = [updatedTask[index-1],updatedTask[index]]
             setTaskList(updatedTask)
         }
     }
-    function moveDown(index){
-        const updatedTask = [...taskList]
-        if (index < updatedTask.length - 1){
-            [updatedTask[index + 1], updatedTask[index]] = [updatedTask[index], updatedTask[index + 1]] 
+    function moveDown(id){
+        const updatedTask = [...taskList];
+        const index = updatedTask.findIndex(item=> item.id === id);
+        if (index < taskList.length - 1){
+            [updatedTask[index+1], updatedTask[index]] = [updatedTask[index], updatedTask[index+1]]
             setTaskList(updatedTask)
         }
     }
-return(
-    <section className="todo_container">
-        {alert === "idle" && <h1>Welcome Back!</h1>}
-        {alert === "blank" && <span className="alertUser">You need to enter a task!</span>}
-        {alert === "duplicate" && <span className="alertUser">That task exist already!</span>}
-        <form onSubmit={handleSubmit}>
-            <section className="taskGroup">
-                <input type="text" name="task" value={task} onChange={handleChange}
-                    placeholder="Add task here..." />
-                <button>ADD</button>
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+            <section className='todo_container'>
+                <section className="alertSection">
+                    {alert === "idle" && <h1>Welcome back!</h1>}
+                    {alert === "duplicate" && <span className="alertUser">Already exists!</span>}
+                    {alert === "blank" && <span className="alertUser">You cannot add task with blank space!</span>}
+                </section>
+                <section className='taskGroup'>
+                    <input type="text" name="task" value={task} onChange={handleChange} placeholder="Enter your task here..." />
+                    <button type="submit">ADD</button>
+                </section>
+                <section className="tasks">
+                    <ul>
+                        {taskList.map((task,id) => 
+                        <li key={task.id}> {task.text}
+                            <section className='listBtn'>
+                                <button type="button" onClick={()=> moveUp(task.id)}>☝️</button>
+                                <button type="button" onClick={()=> moveDown(task.id)}>👇</button>
+                                <button type="button" onClick={()=> handleDelete(task.id)}>❌</button>    
+                            </section>
+                        </li>)}
+                    </ul>
+                </section>
             </section>
-        </form>
-        <section className="tasks">
-            <ul>
-                {taskList.map((task, index) => (
-                <li key={task.id}> {task.text}
-                    <section className="listBtn">
-                        <button type="button" onClick={()=> moveUp(index)}>☝️</button>
-                        <button type="button" onClick={()=> moveDown(index)}>👇</button>
-                        <button type="button" onClick={()=>handleDelete(index)}>❌</button>
-                    </section>
-                </li> ))}
-            </ul>
+            </form>
         </section>
-    </section>
-)}
+    )
+}
